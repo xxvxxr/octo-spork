1e894b74c0e2bff08c2e7e4fb9452e6eeccbb021
Xavier Bradley <xvrbradley@gmail.com>
2022-01-28 10:30:04 -0700

    Completed but need to pull the original and check spot the differences

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 // Using the key word function at the top level allows the browser to give more detailed infromation to you
 function getMessages(req, res) {
-	res.sendFile(path.join(__dirname, '..', 'Public', 'skimountain.jpg'))
+	res.sendFile(path.join(__dirname, '..', 'Public', 'images', 'skimountain.jpg'))
 }
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 
 
 const colors = require('colors')
@@ -9,15 +10,25 @@ const app = express()
 
 const PORT = 3000
 
+app.set('view engine', 'hbs')
+app.set('views',path.join(__dirname, 'views') )
+
 app.use((req, res, next) => {
 	const startTime = Date.now()
 	next()
 	const delta = Date.now() - startTime
 	console.log(`${req.method} and "${req.baseUrl}" ${req.url} @ ${delta}ms`)
 })
-
+app.use('/site', express.static(path.join(__dirname,'Public')))
 app.use(express.json())
 
+
+app.get('./', (req, res) => {
+	res.render('index', {
+		title: 'My friends are clever', 
+		caption: `Let's go Skiiing`
+	})
+})
 app.use('/friends', friendsRouter)
 app.use('/messages', messageRouter)
 
